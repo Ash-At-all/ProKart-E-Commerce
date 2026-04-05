873ac1c93a88521e40ba21a926d8c6da5d99b335
Ash_04 <mujahidshaikh8312@gmail.com>
2026-04-05 19:09:51 +0530

    Add files via upload

---
+++ mongo.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = MongoClient(MONGO_URL)
+db = client["prokart"]
+
+cart_collection = db["cart"]
